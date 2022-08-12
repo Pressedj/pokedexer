98761e77e1c51924f0b3a2b1f8f96af4b71784cb
Juniper <jpress.box@gmail.com>
2022-08-12 12:48:39 -0400

    draft pokedex without css and voice synthesis

--- js/main.js
+++ js/main.js
@@ -1,44 +1,53 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 //Example fetch using pokemonapi.co
-document.getElementById('cocktailer').addEventListener('click', searchCocktail)
-document.getElementById('next').addEventListener('click', nextCocktail)
-document.getElementById('prev').addEventListener('click', prevCocktail)
+document.getElementById('poke').addEventListener('click', getFetch)
+/*document.getElementById('next').addEventListener('click', nextCocktail)
+document.getElementById('prev').addEventListener('click', prevCocktail)*/
 
-let i = 0
+/*let i = 0
+*/
 function getFetch() {
-    let  choice = document.querySelector('input').value
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + choice
-
-    fetch(url)
+    let choice = document.querySelector('input').value
+    const url1 = 'https://pokeapi.co/api/v2/pokemon/' + choice
+    const url2 = 'https://pokeapi.co/api/v2/pokemon-species/' + choice
+    fetch(url1)
         .then(res => res.json()) // pasrse response as JSON
         .then(data => {
-            console.log(data.drinks[i])
-            val = data.drinks[i]
-            document.querySelector('h2').innerHTML = val.strDrink
-            document.querySelector('p').innerHTML = val.strInstructions
-            document.querySelector('img').src = val.strDrinkThumb
+            console.log(data)
+            document.querySelector('h2').innerHTML = data.name
+            document.querySelector('img').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + data.id + ".png"
+            document.getElementById('type').innerHTML = data.types[0].type.name + '-type Pokemon'
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
-    return i
+    fetch(url2)
+        .then(res => res.json()) // pasrse response as JSON
+        .then(data => {
+            console.log(data)
+            document.querySelector('p').innerHTML = data.flavor_text_entries[0].flavor_text
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+
 }
 
-function searchCocktail() {
-    i = 0
-    getFetch()
-    document.getElementById('info').style.display = 'block';
-    return i
-}
-
-function nextCocktail() {
+/*    function searchCocktail() {
+        i = 0
+        getFetch()
+        document.getElementById('info').style.display = 'block';
+        return i
+    }
+*/
+/*function nextCocktail() {
     i ++
     getFetch()
     return i
 
-}
+}*/
 
-function prevCocktail() {
+/*function prevCocktail() {
     if (i > 0) {
         i--
         getFetch()
@@ -47,19 +56,4 @@ function prevCocktail() {
     }
 
 }
-/*function getFetch() {
-    const choice = document.querySelector('img')
-    const url = 'https://dog.ceo/api/breeds/image/random'
-
-    fetch(url)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-            console.log(data)
-            choice.src = data.message
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
-
-}*/
-
+*/
